9e89c97dfb589b75344dc387be67a4cd4820a8d8
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-07-30 16:24:57 -0300

    env vars

--- index.ts
+++ index.ts
@@ -2,6 +2,12 @@ import * as express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/env", (req, res) => {
+  res.json({
+    environment: process.env.NODE_ENV,
+  });
+});
+
 app.get("/hola", (req, res) => {
   res.json({
     message: "hola soy marce",
