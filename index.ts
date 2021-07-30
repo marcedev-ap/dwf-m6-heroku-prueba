c839f37b9ef78eb7615d241ed0160bcf9323f6d6
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-07-30 16:29:52 -0300

    env vars 2

--- index.ts
+++ index.ts
@@ -7,6 +7,11 @@ app.get("/env", (req, res) => {
     environment: process.env.NODE_ENV,
   });
 });
+app.get("/db-env", (req, res) => {
+  res.json({
+    "db-host": process.env.DB_HOST,
+  });
+});
 
 app.get("/hola", (req, res) => {
   res.json({
